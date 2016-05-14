a4e33cf32baad682d4b8a7f789da3d56d81713cf
Pradeep Peiris <wpnpeiris@gmail.com>
2016-05-14 18:41:41 +0200

    add place icons

--- src/js/gmap/map.js
+++ src/js/gmap/map.js
@@ -3,7 +3,24 @@ var app = app || {};
 
 app.GoogleMap = function() {
 
+	
 	this.init = function() {
+		var MAP_DATA = new Object();
+		MAP_DATA['atm'] = {type:SHIELD,  color: "#1998F7", icon: "map-icon-atm"};
+		MAP_DATA['bank'] = {type:SHIELD,  color: "#1998F7", icon: "map-icon-bank"};
+		MAP_DATA['cafe'] = {type:SQUARE_ROUNDED,  color: "#1998F7", icon: "map-icon-cafe"};
+		MAP_DATA['dentist'] = {type:SHIELD,  color: "#1998F7", icon: "map-icon-dentist"};
+		MAP_DATA['grocery_or_supermarket'] = {type:SQUARE_ROUNDED,  color: "#6331AE", icon: "map-icon-grocery-or-supermarket"};
+		MAP_DATA['hospital'] = {type:SHIELD,  color: "#FA8072", icon: "map-icon-health"};
+		MAP_DATA['park'] = {type:ROUTE,  color: "#1998F7", icon: "map-icon-parking"};
+		MAP_DATA['parking'] = {type:ROUTE,  color: "#1998F7", icon: "map-icon-parking"};
+		
+		MAP_DATA['library'] = {type:SHIELD,  color: "#00CCBB", icon: "map-icon-library"};
+		MAP_DATA['restaurant'] = {type:SQUARE_ROUNDED,  color: "#1998F7", icon: "map-icon-restaurant"};
+		
+		MAP_DATA['other'] = {type:MAP_PIN,  color: "#6331AE", icon: "map-icon-location-arrow"};
+		
+		
 		navigator.geolocation.getCurrentPosition(function(location) {
 			var lat = location.coords.latitude;
 			var lng = location.coords.longitude;
@@ -13,29 +30,52 @@ app.GoogleMap = function() {
 			    scrollwheel: false,
 			    zoom: 15	
 			});
-			
 
 			var request = {
 				location : location,
-				radius : '1000',
-				types: ['restaurant', 'store', 'parking']
+				radius : '2000',
+				types: ['atm', 'bank', 'cafe', 'dentist', 'park', 'parking', 'grocery_or_supermarket', 'hospital', 'library', 'restaurant', 'university']
 			};
-
+			
+			var infowindow = new google.maps.InfoWindow();
 			var service = new google.maps.places.PlacesService(map);
+			
 			service.nearbySearch(request, function(results, status) {
 				if (status == google.maps.places.PlacesServiceStatus.OK) {
 					results.forEach(function(place) {
-						place.name;
-						new google.maps.Marker({
-							map : map,
-							icon : {
-								url: place.icon,
-								anchor: new google.maps.Point(10, 10),
-								scaledSize: new google.maps.Size(12, 12)
-								},
-							title : place.name,
-							position : place.geometry.location
+						var photos = place.photos;
+						var p_type = place.types[0];
+						if(!MAP_DATA[p_type]) {
+							p_type = 'other';
+						}
+						
+						var marker = new Marker({
+							map: map,
+							position: place.geometry.location,
+							icon: {
+								path: MAP_DATA[p_type].type,
+								fillColor: MAP_DATA[p_type].color,
+								fillOpacity: 1,
+								strokeColor: '',
+								strokeWeight: 0,
+								scale: 0.5
+							},
+							map_icon_label: '<span class="map-icon ' +  MAP_DATA[p_type].icon + '"></span>'
 						});
+
+						google.maps.event.addListener(marker, 'click',
+								function() {
+									var content = '<div><strong>'
+										+ place.name + '</strong><br>';
+									if(photos) {
+										content += '<img src="'+ photos[0].getUrl({'maxWidth' : 60, 'maxHeight' : 60 }) + '" ><br>';
+									}
+									content += 'Type: ' + place.types[0] + '<br>';
+									content += 'Location: ' + place.vicinity + '</div>';
+										
+									infowindow.setContent(content);
+									infowindow.open(map, this);
+								});
 					});
 				}
 			});
